b3a90d49fe1c85953a92819d382e4bfb9b0d59ab
jake <minecrafterxxl3309@gmail.com>
2026-05-12 13:18:44 +0200

    gui: factored out mainPane into separate class

--- src/minesweeper/gui/GameJGUI.java
+++ src/minesweeper/gui/GameJGUI.java
@@ -1,23 +1,58 @@
 package minesweeper.gui;
 
-import minesweeper.game.*;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import minesweeper.game.*;
+
+// TODO: scale gui based on screen resolution
+// TODO: scroll bars
 // TODO: gui field for Game
+    // probably should do a big refactor to tie the logic and gui together better
 // TODO: flag toggle switch
-// TODO: menu around main frame
-// TODO: investigate javax.swing.Action interface for button state
+// TODO: top bar
 public class GameJGUI extends JFrame {
 
     private Game g;
-    private Container mainPane;
+    private GameMainPane mainPane;
+    private JScrollPane scrollPane;
     private String windowTitle;
     private int boardWidth;
     private int boardHeight;
     private int mines;
+
+    public GameJGUI(String title, Game game) {
+        this.g           = game;
+        this.windowTitle = title;
+        this.boardWidth  = game.getWidth();
+        this.boardHeight = game.getHeight();
+        this.mines       = game.getMines();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle(windowTitle);
+
+        this.mainPane = new GameMainPane(g);
+
+        GameMenuItem exit = new GameMenuItem("Exit", e -> System.exit(0));
+        GameMenuItem restart = new GameMenuItem("Restart", new RestartFunction());
+
+        JMenuBar menuBar  = new JMenuBar();
+        menuBar.add(new GameMenu("Game", new GameMenuItem[] {
+            exit,
+            restart,
+        }));
+        setJMenuBar(menuBar);
+        scrollPane = new JScrollPane(mainPane);
+        scrollPane.add(mainPane);
+        getContentPane().add(scrollPane);
+
+        setTitle(title);
+        setSize(1500, 800);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        pack();
+    }
 
     private class GamePopup extends JDialog {
 
@@ -57,223 +92,19 @@ public class GameJGUI extends JFrame {
         }
     }
 
-    private class MinesweeperButton extends JButton {
-
-        private int x;
-        private int y;
-        private boolean swept = false;
-
-        public MinesweeperButton(int x, int y) {
-            super();
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getCoordX() {
-            return this.x;
-        }
-
-        public int getCoordY() {
-            return this.y;
-        }
-
-        public void sweepButton() {
-            swept = true;
-            setFocusable(false);
-            setFocusPainted(false);
-            setBorderPainted(false);
-            setModel(new DefaultButtonModel() {
-                public boolean isPressed()  { return false; }
-                public boolean isRollover() { return false; }
-                public void setRollover(boolean b) {}
-            });
-
-            int cellText = g.getAdjacencyBoard().getCell(this.x, this.y);
-            JLabel bLabel = new JLabel(cellText == 0 ? "" : cellText + "", JLabel.CENTER);
-            Color c;
-
-            switch (cellText) {
-                default -> c = Color.BLACK;
-                case 1  -> c = Color.BLUE;
-                case 2  -> c = Color.GREEN;
-                case 3  -> c = Color.RED;
-                case 4  -> c = Color.BLUE.darker();
-                case 5  -> c = Color.RED.darker();
-                case 6  -> c = Color.CYAN;
-                case 7  -> c = Color.MAGENTA.darker();
-                case 8  -> c = Color.LIGHT_GRAY;
-            }
-            bLabel.setForeground(c);
-            add(bLabel);
-            for (MouseListener a : getMouseListeners()) {
-                removeMouseListener(a);
-            }
-
-            addMouseListener(new SweptButtonListener());
-        }
-
-        // TODO: only run this if the cell is satisfied
-        public void click() {
-            if (swept) return;
-            MouseEvent e = new MouseEvent(this, MouseEvent.MOUSE_CLICKED, 0, 0, getX(), getY(), 1, false, 1);
-            processMouseEvent(e);
-        }
-    }
-
-    // button callback
-    private class ButtonListener implements MouseListener {
-
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                MinesweeperButton source = (MinesweeperButton) e.getSource();
-                int result;
-                String newButtonLabel;
-                MinesweeperButton btn;
-
-                switch (e.getButton()) {
-                    case MouseEvent.BUTTON1:
-                        result = g.doAction(source.getCoordX(), source.getCoordY(), 1);
-                        if (result < 0) break;
-
-                        for (Component cmp : mainPane.getComponents()) { // "recursively" delete buttons over 0 cells
-
-                            if (cmp instanceof JLabel) continue;
-
-                            btn = (MinesweeperButton) cmp;
-
-                            if (g.getActionBoard().getCell(btn.x, btn.y) == 1) {
-                                btn.sweepButton();
-                            }
-                        }
-                        validate();
-                        break;
-
-                    case MouseEvent.BUTTON2:
-                        result = g.doAction(source.getCoordX(), source.getCoordY(), 3);
-                        if (result < 0) break;
-
-                        newButtonLabel = "?";
-                        newButtonLabel = (g.getActionBoard().getCell(source.getCoordX(), source.getCoordY()) == 3 ? newButtonLabel : "");
-
-                        btn = (MinesweeperButton) source;
-                        btn.setText(newButtonLabel);
-
-                        validate();
-                        break;
-
-                    case MouseEvent.BUTTON3:
-                        result = g.doAction(source.getCoordX(), source.getCoordY(), 2);
-                        if (result < 0) break;
-
-                        newButtonLabel = "P";
-                        newButtonLabel = (g.getActionBoard().getCell(source.getCoordX(), source.getCoordY()) == 2 ? newButtonLabel : "");
-
-                        btn = (MinesweeperButton) source;
-                        btn.setText(newButtonLabel);
-
-                        validate();
-                        break;
-                }
-
-                if (g.getGameState() != 0) winLosePopup();
-            }
-
-            public void mouseEntered(MouseEvent e)  { }
-            public void mouseExited(MouseEvent e)   { }
-            public void mousePressed(MouseEvent e)  { }
-            public void mouseReleased(MouseEvent e) { }
-    }
-
-    private class SweptButtonListener extends ButtonListener {
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            if (e.getButton() != MouseEvent.BUTTON1) return;
-            int sourceX = ((MinesweeperButton)e.getSource()).x;
-            int sourceY = ((MinesweeperButton)e.getSource()).y;
-
-            if (Board.countNeighbors(g.getActionBoard(), 2, sourceX, sourceY)
-             >= g.getAdjacencyBoard().getCell(sourceX, sourceY)) {
-                for (int[] coord : Board.getNeighbors(sourceX, sourceY)) {
-                    int btnX = coord[0];
-                    int btnY = coord[1];
-                    if (btnX < 0 || btnY < 0 || btnX >= g.getWidth() || btnY >= g.getHeight()) continue;
-                    Component cmp = getComponentByCoordinate(btnX, btnY);
-                    if (!(cmp instanceof MinesweeperButton)) continue;
-                    MinesweeperButton btn = (MinesweeperButton) cmp;
-                    btn.click();
-                }
-            } else {
-            }
-        }
-    }
-
-    private void winLosePopup() {
+    protected void winLosePopup() {
         new GamePopup(this, g.getGameState());
-    }
-
-    private JPanel initMainPane(String title, int width, int height, int mines) {
-        var mainPane = new JPanel();
-
-        mainPane.setLayout(new GridLayout(height, width));
-        mainPane.setPreferredSize(new Dimension(width * 50, height * 50));
-
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-
-                MinesweeperButton btn = new MinesweeperButton(j, i);
-
-                btn.setPreferredSize(new Dimension(40, 40));
-                btn.addMouseListener(new ButtonListener());
-
-                mainPane.add(btn);
-            }
-        }
-        return mainPane;
-    }
-
-    public GameJGUI(String title, Game game) {
-        this.g           = game;
-        this.windowTitle = title;
-        this.boardWidth  = game.getWidth();
-        this.boardHeight = game.getHeight();
-        this.mines       = game.getMines();
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        this.mainPane = initMainPane(windowTitle, boardWidth, boardHeight, mines);
-
-        GameMenuItem exit = new GameMenuItem("Exit", e -> System.exit(0));
-        GameMenuItem restart = new GameMenuItem("Restart", new RestartFunction());
-
-        JMenuBar menuBar  = new JMenuBar();
-        menuBar.add(new GameMenu("Game", new GameMenuItem[] {
-            exit,
-            restart,
-        }));
-        setJMenuBar(menuBar);
-        getContentPane().add(mainPane);
-
-        setTitle(title);
-        setSize(1500, 800);
-        setLocationRelativeTo(null);
-        setVisible(true);
-        pack();
     }
 
     public GameJGUI(String title, int width, int height, int mines) {
         this(title, new Game(width, height, mines));
     }
 
-    public Component getComponentByCoordinate(int x, int y) {
-        return (Component) mainPane.getAccessibleContext().getAccessibleChild(y * g.getWidth() + x);
-    }
-
     private class RestartFunction implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
             remove(mainPane);
-            mainPane = initMainPane(windowTitle, boardWidth, boardHeight, mines);
+            mainPane = new GameMainPane(g);
             g = new Game(boardWidth, boardHeight, mines);
             getContentPane().add(mainPane);
             validate();
@@ -284,6 +115,7 @@ public class GameJGUI extends JFrame {
 
         try {
             // making it not look terrible
+            // TODO: custom look and feel
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
         } catch(Exception ignored) {}
 
---
+++ src/minesweeper/gui/GameMainPane.java
@@ -0,0 +1,216 @@
+package minesweeper.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import minesweeper.game.*;
+
+public class GameMainPane extends JPanel implements Scrollable {
+
+    private Game g;
+    private int boardHeight;
+    private int boardWidth;
+
+    public GameMainPane(Game game) {
+        super();
+        boardHeight = game.getHeight();
+        boardWidth = game.getWidth();
+
+        setLayout(new GridLayout(boardHeight, boardWidth));
+        setPreferredSize(new Dimension(boardWidth * 50, boardHeight * 50));
+
+        for (int i = 0; i < boardHeight; i++) {
+            for (int j = 0; j < boardWidth; j++) {
+
+                MinesweeperButton btn = new MinesweeperButton(j, i);
+
+                btn.setPreferredSize(new Dimension(40, 40));
+                btn.addMouseListener(new ButtonListener());
+
+                add(btn);
+            }
+        }
+    }
+
+    private class MinesweeperButton extends JButton {
+
+        private int x;
+        private int y;
+        private boolean swept = false;
+
+        public MinesweeperButton(int x, int y) {
+            super();
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getCoordX() {
+            return this.x;
+        }
+
+        public int getCoordY() {
+            return this.y;
+        }
+
+        public void sweepButton() {
+            swept = true;
+            setFocusable(false);
+            setFocusPainted(false);
+            setBorderPainted(false);
+            setModel(new DefaultButtonModel() {
+                public boolean isPressed()  { return false; }
+                public boolean isRollover() { return false; }
+                public void setRollover(boolean b) {}
+            });
+
+            setText("");
+            int cellText = g.getAdjacencyBoard().getCell(this.x, this.y);
+            JLabel bLabel = new JLabel(cellText == 0 ? "" : cellText + "", JLabel.CENTER);
+            Color c;
+            switch (cellText) {
+                default -> c = Color.BLACK;
+                case 1  -> c = Color.BLUE;
+                case 2  -> c = Color.GREEN;
+                case 3  -> c = Color.RED;
+                case 4  -> c = Color.BLUE.darker();
+                case 5  -> c = Color.RED.darker();
+                case 6  -> c = Color.CYAN;
+                case 7  -> c = Color.MAGENTA.darker();
+                case 8  -> c = Color.LIGHT_GRAY;
+            }
+            bLabel.setForeground(c);
+            add(bLabel);
+            for (MouseListener a : getMouseListeners()) {
+                removeMouseListener(a);
+            }
+
+            addMouseListener(new SweptButtonListener());
+        }
+
+        // TODO: only run this if the cell is satisfied
+        public void click() {
+            if (swept) return;
+            MouseEvent e = new MouseEvent(this, MouseEvent.MOUSE_CLICKED, 0, 0, getX(), getY(), 1, false, 1);
+            processMouseEvent(e);
+        }
+    }
+
+    // button callback
+    private class ButtonListener implements MouseListener {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                MinesweeperButton source = (MinesweeperButton) e.getSource();
+                int result;
+                String newButtonLabel;
+                MinesweeperButton btn;
+
+                switch (e.getButton()) {
+                    case MouseEvent.BUTTON1:
+                        result = g.doAction(source.getCoordX(), source.getCoordY(), 1);
+                        if (result < 0) break;
+
+                        for (Component cmp : getComponents()) { // "recursively" delete buttons over 0 cells
+
+                            if (cmp instanceof JLabel) continue;
+
+                            btn = (MinesweeperButton) cmp;
+
+                            if (g.getActionBoard().getCell(btn.x, btn.y) == 1) {
+                                btn.sweepButton();
+                            }
+                        }
+                        validate();
+                        break;
+
+                    case MouseEvent.BUTTON2:
+                        result = g.doAction(source.getCoordX(), source.getCoordY(), 3);
+                        if (result < 0) break;
+
+                        newButtonLabel = "?";
+                        newButtonLabel = (g.getActionBoard().getCell(source.getCoordX(), source.getCoordY()) == 3 ? newButtonLabel : "");
+
+                        btn = (MinesweeperButton) source;
+                        btn.setText(newButtonLabel);
+
+                        validate();
+                        break;
+
+                    case MouseEvent.BUTTON3:
+                        result = g.doAction(source.getCoordX(), source.getCoordY(), 2);
+                        if (result < 0) break;
+
+                        newButtonLabel = "P";
+                        newButtonLabel = (g.getActionBoard().getCell(source.getCoordX(), source.getCoordY()) == 2 ? newButtonLabel : "");
+
+                        btn = (MinesweeperButton) source;
+                        btn.setText(newButtonLabel);
+
+                        validate();
+                        break;
+                }
+
+                // HACK: sloppy
+                if (g.getGameState() != 0) ((GameJGUI) getParent()).winLosePopup();
+            }
+
+            public void mouseEntered(MouseEvent e)  { }
+            public void mouseExited(MouseEvent e)   { }
+            public void mousePressed(MouseEvent e)  { }
+            public void mouseReleased(MouseEvent e) { }
+    }
+
+    private class SweptButtonListener extends ButtonListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if (e.getButton() != MouseEvent.BUTTON1) return;
+            int sourceX = ((MinesweeperButton)e.getSource()).x;
+            int sourceY = ((MinesweeperButton)e.getSource()).y;
+
+            if (Board.countNeighbors(g.getActionBoard(), 2, sourceX, sourceY)
+             >= g.getAdjacencyBoard().getCell(sourceX, sourceY)) {
+                for (int[] coord : Board.getNeighbors(sourceX, sourceY)) {
+                    int btnX = coord[0];
+                    int btnY = coord[1];
+                    if (btnX < 0 || btnY < 0 || btnX >= g.getWidth() || btnY >= g.getHeight()) continue;
+                    Component cmp = getComponentByCoordinate(btnX, btnY);
+                    if (!(cmp instanceof MinesweeperButton)) continue;
+                    MinesweeperButton btn = (MinesweeperButton) cmp;
+                    btn.click();
+                }
+            } else {
+            }
+        }
+    }
+
+    private Component getComponentByCoordinate(int x, int y) {
+        return (Component) getAccessibleContext().getAccessibleChild(y * g.getWidth() + x);
+    }
+
+    @Override
+    public Dimension getPreferredScrollableViewportSize() {
+        throw new UnsupportedOperationException("Unimplemented method 'getPreferredScrollableViewportSize'");
+    }
+
+    @Override
+    public int getScrollableBlockIncrement(Rectangle arg0, int arg1, int arg2) {
+        throw new UnsupportedOperationException("Unimplemented method 'getScrollableBlockIncrement'");
+    }
+
+    @Override
+    public boolean getScrollableTracksViewportHeight() {
+        throw new UnsupportedOperationException("Unimplemented method 'getScrollableTracksViewportHeight'");
+    }
+
+    @Override
+    public boolean getScrollableTracksViewportWidth() {
+        throw new UnsupportedOperationException("Unimplemented method 'getScrollableTracksViewportWidth'");
+    }
+
+    @Override
+    public int getScrollableUnitIncrement(Rectangle arg0, int arg1, int arg2) {
+        throw new UnsupportedOperationException("Unimplemented method 'getScrollableUnitIncrement'");
+    }
+}
